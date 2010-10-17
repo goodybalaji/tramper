42651c28ca62bd5ed4f2ddfb0af93b77245b3df0
peveuve <peveuve@194daa44-fb1a-11de-813e-e90011bb59b3>
2010-10-17 14:03:01 +0000

    redirect the help page to the Tramper home page

--- trunk/Tramper/sources/core/org/tramper/action/LoadHelpAction.java
+++ trunk/Tramper/sources/core/org/tramper/action/LoadHelpAction.java
@@ -1,10 +1,12 @@
 package org.tramper.action;
 
 import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.AbstractAction;
 
+import org.apache.log4j.Logger;
 import org.tramper.doc.Library;
 import org.tramper.doc.Target;
 import org.tramper.loader.Loader;
@@ -17,6 +19,8 @@ import org.tramper.loader.LoaderFactory;
 public class LoadHelpAction extends AbstractAction {
     /** LoadHelpAction.java long */
     private static final long serialVersionUID = 165520303630403098L;
+    /** logger */
+    private Logger logger = Logger.getLogger(LoadHelpAction.class);
     /** singleton */
     private static LoadHelpAction instance;
     
@@ -42,8 +46,12 @@ public class LoadHelpAction extends AbstractAction {
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
      */
     public void actionPerformed(ActionEvent e) {
-	Loader loader = LoaderFactory.getInstance().newLoader();
-	URL helpUrl = this.getClass().getResource("/org/tramper/doc/help.html");
-	loader.download(helpUrl.toString(), new Target(Library.PRIMARY_FRAME, null));
+	try {
+	    URL helpUrl = new URL("http://sites.google.com/site/tramperproject/");
+	    Loader loader = LoaderFactory.getInstance().newLoader();
+	    loader.download(helpUrl.toString(), new Target(Library.PRIMARY_FRAME, null));
+	} catch (MalformedURLException ex) {
+	    logger.error("help page unavailable");
+	}
     }
 }
